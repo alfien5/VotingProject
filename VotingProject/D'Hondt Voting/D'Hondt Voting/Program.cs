844a91a73d9795927f7d20be072fc1bb18db3a6b
SimpleTableLamp <79853956+SimpleTableLamp@users.noreply.github.com>
2021-03-01 16:39:15 +0000

    Update Program.cs

--- VotingProject/D'Hondt Voting/D'Hondt Voting/Program.cs
+++ VotingProject/D'Hondt Voting/D'Hondt Voting/Program.cs
@@ -6,7 +6,7 @@ namespace D_Hondt_Voting
     {
         static void Main(string[] args)
         {
-            Console.Writeline("Hello World!")
+            Console.Writeline("Hello World! Alfie has big brain")
         }
     }
 }
