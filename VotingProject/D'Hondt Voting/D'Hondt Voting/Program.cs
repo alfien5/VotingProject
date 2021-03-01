d5b2cbb0efb027b2902b3301fdd377725c0c0369
Sophie Chau <79196793+Feathers95@users.noreply.github.com>
2021-03-01 16:42:45 +0000

    Damn commit

--- VotingProject/D'Hondt Voting/D'Hondt Voting/Program.cs
+++ VotingProject/D'Hondt Voting/D'Hondt Voting/Program.cs
@@ -7,6 +7,7 @@ namespace D_Hondt_Voting
         static void Main(string[] args)
         {
             Console.Writeline("Hello World! Alfie has not got a big brain")
+            Console.Writeline("Damn.")
         }
     }
 }
