ca95accc3ad736f9c40d37cb1978157a9200c0ad
Alfie Newton <63203898+a1fienewt0n@users.noreply.github.com>
2021-03-02 17:00:19 +0000

    Update Program.cs

--- VotingProject/D'Hondt Voting/D'Hondt Voting/Program.cs
+++ VotingProject/D'Hondt Voting/D'Hondt Voting/Program.cs
@@ -1,14 +1,47 @@
-﻿using System;
+using System;
+using System.IO;
+using System.Collections.Generic;
 
-namespace D_Hondt_Voting
+public class Get_Votes
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string fileName = @"C:\Users\newto\Desktop\VotingProject\Votes.txt"; //assigns the text file to a string
+
+        using (StreamReader reader = new StreamReader(fileName)) // Opens an instance of the file
         {
-            Console.Writeline("Hello World! Alfie has not got a big brain")
-            Console.Writeline("Damn.")
-            Console.Writeline("Alfie's a bad team leader")
+            string line;
+            int counter = 0;
+            List<string> parties = new List<string>();
+            List<int> party_votes = new List<int>(); // Variables for the lines of the text file, a counter and a couple lists
+
+            while ((line = reader.ReadLine()) != null) // Do while the line of the text file is not empty
+            {
+                if (counter == 1) // check for the line in the text file with the total seats on it
+                {
+                    int total_seats = System.Convert.ToInt32(line); // Assigns a integer version of the text file's line to a variable
+                    Console.WriteLine(total_seats); // Just to check it has the correct info
+                }
+
+                if (counter == 2) // check for the line in the text file with the total votes on it
+                {
+                    int total_votes = System.Convert.ToInt32(line);
+                    Console.WriteLine(total_votes);
+                }
+
+                if (counter > 2) // The rest of the text file's lines are parties and their votes
+                {
+                    parties.Add(line.Substring(0, line.IndexOf(","))); // Gets the name of the party and adds it to the 'parties' list
+                    Console.WriteLine(line.Substring(0, line.IndexOf(","))); // Displays the party name
+                    line = line.Substring(line.IndexOf(",")+ 1); // Cuts the name of the party off the string
+                    party_votes.Add(System.Convert.ToInt32(line.Substring(0, line.IndexOf(",")))); // Retrieves the partie's votes and adds them to a list
+                    Console.WriteLine(System.Convert.ToInt32(line.Substring(0, line.IndexOf(",")))); // Displays the partie's votes
+
+                }
+
+                counter += 1; // Makes sure the next line of the file is read
+            }
+
         }
     }
 }
