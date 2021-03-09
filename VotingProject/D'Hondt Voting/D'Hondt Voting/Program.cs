674a31bf870ee1161897581a750e79d6759d3b6f
Sophie Chau <sophiechau@hotmail.co.uk>
2021-03-09 09:38:37 +0000

    Created dictionary to hold all created party objects with integer keys.

--- VotingProject/D'Hondt Voting/D'Hondt Voting/Program.cs
+++ VotingProject/D'Hondt Voting/D'Hondt Voting/Program.cs
@@ -5,12 +5,11 @@ using System.Collections.Generic;
 public class Get_Votes
 {
     public static List<string> parties = new List<string>();
-    public static List<int> party_votes = new List<int>();
-    public static List<string> seats = new List<string>();// Variables for the lines of the text file, a counter and a couple lists
+    public static List<int> party_votes = new List<int>(); // Variables for the lines of the text file, a counter and a couple lists
 
     public static void ReadFiles()
     {
-        string fileName = @"C:\Users\newto\Desktop\VotingProject\Votes.txt"; //assigns the text file to a string
+        string fileName = @"Votes.txt"; //assigns the text file to a string
 
         using (StreamReader reader = new StreamReader(fileName)) // Opens an instance of the file
         {
@@ -22,22 +21,22 @@ public class Get_Votes
                 if (counter == 1) // check for the line in the text file with the total seats on it
                 {
                     int total_seats = System.Convert.ToInt32(line); // Assigns a integer version of the text file's line to a variable
-                    Console.WriteLine(total_seats); // Just to check it has the correct info
+                    //Console.WriteLine(total_seats); // Just to check it has the correct info
                 }
 
                 if (counter == 2) // check for the line in the text file with the total votes on it
                 {
                     int total_votes = System.Convert.ToInt32(line);
-                    Console.WriteLine(total_votes);
+                    //Console.WriteLine(total_votes);
                 }
 
                 if (counter > 2) // The rest of the text file's lines are parties and their votes
                 {
                     parties.Add(line.Substring(0, line.IndexOf(","))); // Gets the name of the party and adds it to the 'parties' list
-                    Console.WriteLine(line.Substring(0, line.IndexOf(","))); // Displays the party name
+                    //Console.WriteLine(line.Substring(0, line.IndexOf(","))); // Displays the party name
                     line = line.Substring(line.IndexOf(",")+ 1); // Cuts the name of the party off the string
                     party_votes.Add(System.Convert.ToInt32(line.Substring(0, line.IndexOf(",")))); // Retrieves the partie's votes and adds them to a list
-                    Console.WriteLine(System.Convert.ToInt32(line.Substring(0, line.IndexOf(",")))); // Displays the partie's votes
+                    //Console.WriteLine(System.Convert.ToInt32(line.Substring(0, line.IndexOf(",")))); // Displays the partie's votes
 
                 }
 
@@ -53,6 +52,7 @@ public class Get_Votes
         public string name;
         public int numVotes;
         public int numWins;
+        public List<string> seatsleft = new List<string>();
 
         //constructor
         public Party(string partyName, int partyNumVotes, int partyNumSeats)
@@ -61,7 +61,7 @@ public class Get_Votes
             numVotes = partyNumVotes;
             for (int i = 1; i != partyNumSeats + 1; i++)
             {
-                seats.Add("SEAT" + i);
+                seatsleft.Add("SEAT" + i);
             }
             numWins = 0;
         }
@@ -70,32 +70,39 @@ public class Get_Votes
         public void SeatWon()
         {
             numWins++;
-            seats.RemoveAt(0);
+            seatsleft.RemoveAt(0);
         }
 
         //!!! FOR TESTING
         public void Check()
         {
-            Console.WriteLine("Party's name = " + name + " | Votes = " + numVotes);
-            for (int i = 0; i < seats.Count; i++)
-                Console.WriteLine(seats[i]);
+            Console.WriteLine("Party's name = " + name);
+            Console.WriteLine("Votes = " + numVotes);
+            Console.WriteLine("Wins = " + numWins);
+            for (int i = 0; i < seatsleft.Count; i++)
+                Console.WriteLine(seatsleft[i]);
         }
+        public Party[] partyObjects;
         public static void Main(string[] args)
         {
-            //!!! FOR TESTING
-            Party party = new Party("BB", 69, 5);
-            party.SeatWon();
-            party.Check();
-
             Get_Votes.ReadFiles();
 
-            //!!! Use TEAM LEADER's lists to create the parties here
-            // Testing for list access - struggling
-            for (int i = 0; i < parties.Count; i++)
-                Console.WriteLine(parties[i]);
-            for (int i = 0; i < party_votes.Count; i++)
-                Console.WriteLine(party_votes[i]);
+            //Creating dictionary for party objects
+            Dictionary<int, Party> partyObjects = new Dictionary<int,Party>();
 
+            //Creating the individual party objects
+            for (int i = 0; i < parties.Count-1; i++)
+            {
+                partyObjects.Add(i, new Party(parties[i], party_votes[i], 5));
+            }
+            //Creation exception for Independent which only has 1 seat
+            partyObjects.Add(8, new Party(parties[8], party_votes[8], 1));
+
+            //Testing each party object's contents
+            for (int i = 0; i < parties.Count; i++)
+                {
+                    partyObjects[i].Check();
+                }
         }
     }
 }
