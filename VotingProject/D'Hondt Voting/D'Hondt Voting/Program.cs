da8530f74d04f21330461e74962295b42a049de4
Alfie Newton <63203898+a1fienewt0n@users.noreply.github.com>
2021-03-09 13:59:03 +0000

    Update Program.cs

--- VotingProject/D'Hondt Voting/D'Hondt Voting/Program.cs
+++ VotingProject/D'Hondt Voting/D'Hondt Voting/Program.cs
@@ -14,29 +14,25 @@ public class Get_Votes
         using (StreamReader reader = new StreamReader(fileName)) // Opens an instance of the file
         {
             string line;
-            int counter = 0;          
+            int counter = 0;
 
             while ((line = reader.ReadLine()) != null) // Do while the line of the text file is not empty
             {
                 if (counter == 1) // check for the line in the text file with the total seats on it
                 {
                     int total_seats = System.Convert.ToInt32(line); // Assigns a integer version of the text file's line to a variable
-                    //Console.WriteLine(total_seats); // Just to check it has the correct info
                 }
 
                 if (counter == 2) // check for the line in the text file with the total votes on it
                 {
                     int total_votes = System.Convert.ToInt32(line);
-                    //Console.WriteLine(total_votes);
                 }
 
                 if (counter > 2) // The rest of the text file's lines are parties and their votes
                 {
                     parties.Add(line.Substring(0, line.IndexOf(","))); // Gets the name of the party and adds it to the 'parties' list
-                    //Console.WriteLine(line.Substring(0, line.IndexOf(","))); // Displays the party name
-                    line = line.Substring(line.IndexOf(",")+ 1); // Cuts the name of the party off the string
+                    line = line.Substring(line.IndexOf(",") + 1); // Cuts the name of the party off the string
                     party_votes.Add(System.Convert.ToInt32(line.Substring(0, line.IndexOf(",")))); // Retrieves the partie's votes and adds them to a list
-                    //Console.WriteLine(System.Convert.ToInt32(line.Substring(0, line.IndexOf(",")))); // Displays the partie's votes
 
                 }
 
@@ -88,10 +84,10 @@ public class Get_Votes
             Get_Votes.ReadFiles();
 
             //Creating dictionary for party objects
-            Dictionary<int, Party> partyObjects = new Dictionary<int,Party>();
+            Dictionary<int, Party> partyObjects = new Dictionary<int, Party>();
 
             //Creating the individual party objects
-            for (int i = 0; i < parties.Count-1; i++)
+            for (int i = 0; i < parties.Count - 1; i++)
             {
                 partyObjects.Add(i, new Party(parties[i], party_votes[i], 5));
             }
@@ -100,9 +96,9 @@ public class Get_Votes
 
             //Testing each party object's contents
             for (int i = 0; i < parties.Count; i++)
-                {
-                    partyObjects[i].Check();
-                }
+            {
+                partyObjects[i].Check();
+            }
         }
     }
 }
